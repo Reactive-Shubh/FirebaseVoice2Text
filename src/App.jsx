12e2af9d7bbc1146977938370f7d4b98408e84f8
Shubh02511 <109104995+Shubh02511@users.noreply.github.com>
2024-04-02 14:21:10 +0530

    Project Completed

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const App = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
+        <p>Microphone: {listening ? "on" : "off"}</p>
+
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {transcript}
+        </div>
+
+        <div className="btn-style">
+          <button onClick={SpeechRecognition.startListening}>Start</button>
+          <button onClick={SpeechRecognition.stopListening}>Stop</button>
+          <button onClick={resetTranscript}>Reset</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
